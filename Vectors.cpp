c0211332d3502c0fe55459ef8f0b353a484541b5
SonicIcarus <jacksinnott1994@gmail.com>
2025-05-28 15:04:41 +0100

    BAsic arithimitic functions finished and tested

--- Vectors.cpp
+++ Vectors.cpp
@@ -2,68 +2,65 @@
 
 // Macro that uses Float Epsilon as a threshold buffer for comparing two floating point numbers.
 // This helps us to determine if they are close enough to be considered equal, accouting for prescison errors.
-#define CMP(x, y)                    \
-    (fabsf((x)–(y)) <= FLT_EPSILON * \
-      fmaxf(1.0f,                    \
-      fmaxf(fabsf(x), fabsf(y)))     \
-   )
+#define CMP(x, y) \
+		(fabsf(x - y) <= FLT_EPSILON * fmaxf(1.0f, fmaxf(fabsf(x), fabsf(y))))
 
 vec2 operator+(const vec2& l, const vec2& r)
 {
-	return vec2();
+	return { l.x + r.x, l.y + r.y };
 }
 
 vec3 operator+(const vec3& l, const vec3& r)
 {
-	return vec3();
+	return { l.x + r.x, l.y + r.y, l.z + r.z };
 }
 
 vec2 operator-(const vec2& l, const vec2& r)
 {
-	return vec2();
+	return { l.x - r.x, l.y - r.y };
 }
 
 vec3 operator-(const vec3& l, const vec3& r)
 {
-	return vec3();
+	return { l.x - r.x, l.y - r.y, l.z - r.z };
 }
 
 vec2 operator*(const vec2& l, const vec2& r)
 {
-	return vec2();
+	return { l.x * r.x, l.y * r.y };
 }
 
 vec3 operator*(const vec3& l, const vec3& r)
 {
-	return vec3();
+	return { l.x * r.x, l.y * r.y, l.z * r.z };
 }
 
 vec2 operator*(const vec2& l, float r)
 {
-	return vec2();
+	return { l.x * r, l.y * r };
 }
 
 vec3 operator*(const vec3& l, float r)
 {
-	return vec3();
+	return { l.x * r, l.y * r, l.z * r };
 }
 
 bool operator==(const vec2& l, const vec2& r)
 {
-	return false;
+	return CMP(l.x, r.x) && CMP(l.y, r.y);
 }
 
 bool operator==(const vec3& l, const vec3& r)
 {
-	return false;
+	return CMP(l.x, r.x) && CMP(l.y, r.y) && CMP(l.z, r.z);
 }
 
 bool operator!=(const vec2& l, const vec2& r)
 {
-	return false;
+	return !(l == r);
 }
 
 bool operator!=(const vec3& l, const vec3& r)
 {
-	return false;
+	return !(l == r);
 }
--- main.cpp
+++ main.cpp
@@ -3,11 +3,57 @@
 
 int main()
 {
-	vec2 vec2 = { 2.0f, 100.0f };
-	vec3 vec3 = { 1.0f, 50.0f, 600.0f };
+	vec2 right = { 2.0f, 100.0f };
+	vec3 left = { 1.0f, 50.0f, 600.0f };
 
-	std::cout << "The vec2 is made up of:\nx: " << vec2.x << "\ny " << vec2.y << "\n\n";
-	std::cout << "The vec3 is made up of:\nx: " << vec3.x << "\ny " << vec3.y << "\nz " << vec3.z << "\n";
+	std::cout << "Vec2\n\n\nThe vec2 is made up of:\nx: " << right.x << "\ny " << right.y << "\n\n";
+	std::cout << "The vec3 is made up of:\nx: " << left.x << "\ny " << left.y << "\nz " << left.z << "\n\n\n";
+
+	vec2 testVec = { 1.0f, 200.0f};
+	vec2 result = right + testVec;
+
+	std::cout << "the result of adding is: " << result.x << " " << result.y << "\n\n\n";
+
+	result = { 0.0f,0.0f };
+
+	result = right - testVec;
+
+	std::cout << "the result of subtracting is: " << result.x << " " << result.y << "\n\n\n";
+
+	result = { 0.0f,0.0f };
+
+	result = right * testVec;
+
+	std::cout << "the result of multiplying by a vector is: " << result.x << " " << result.y << "\n\n\n";
+
+	result = { 0.0f,0.0f };
+
+	result = right * 2;
+
+	std::cout << "the result of multiplying by a scalar is: " << result.x << " " << result.y << "\n\n\nVec3\n\n\n";
+
+	vec3 testVec3 = { 1.0f, 200.0f, 300.0f };
+	vec3 result3 = left + testVec3;
+
+	std::cout << "the result of adding is: " << result3.x << " " << result3.y << " " << result3.z << "\n\n\n";
+
+	result3 = { 0.0f,0.0f,0.0f };
+
+	result3 = left - testVec3;
+
+	std::cout << "the result of subtracting is: " << result3.x << " " << result3.y << " " << result3.z << "\n\n\n";
+
+	result3 = { 0.0f,0.0f,0.0f };
+
+	result3 = left * testVec3;
+
+	std::cout << "the result of multiplying by a vector is: " << result3.x << " " << result3.y << " " << result3.z << "\n\n\n";
+
+	result3 = { 0.0f,0.0f,0.0f };
+
+	result3 = left * 2;
+
+	std::cout << "the result of multiplying by a scalar is: " << result3.x << " " << result3.y << " " << result3.z << "\n\n\n";
 
 	return 0;
 }
